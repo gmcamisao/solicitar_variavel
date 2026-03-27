98d93ffd0c7f4e2b0ebb7e8ace1458e79702ca22
Gustavo Moraes Camisão <gmoraescamisao@gmail.com>
2026-03-27 17:25:46 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/variavel/Principal.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class Principal {
+
+    public static void main(String[] args) {
+        int Valor;
+        Valor = 1;
+        JOptionPane.showMessageDialog(null,"O valor da variável é = " + Valor);
+        
+    }
+}
